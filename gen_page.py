5d10f042ee1c944ff87ec49a7fbb1daa03614eaf
xanthics <3137623+xanthics@users.noreply.github.com>
2021-08-27 17:26:12 -0700

    query string support

--- gen_page.py
+++ gen_page.py
@@ -33,7 +33,7 @@ def init_page():
 		data = load(f)
 	nodes = defaultdict(list)
 	for domain in ['Special', 'Force', 'Entropy', 'Form', 'Inertia', 'Life']:
-		s = SECTION(H1(domain), Class=domain.lower())
+		s = SECTION(H1(domain), Id=domain.lower(), Class=domain.lower())
 		for item in data[domain]:
 			data_value = min_search_coverage([item['name']] + [z for x in item['nodes'] for y in x for z in y] + [y for x in item['implicit'] for y in x if y])
 			t = TABLE(COL(Class='first_column') + COL() + COL(), Class='onehundred borders', data_value=data_value, data_asc_id=item["name"].replace(" ", "_"))
@@ -49,7 +49,7 @@ def init_page():
 					nodes[n].append((domain, item['name'], c, 0))
 					if item['implicit'][c]:
 						nodes[', '.join(item['implicit'][c])].append((domain, item['name'], c))
-					t <= TR(TD(f"{c+1}: " + SELECT((OPTION(x, value=f"{x}") for x in ['Any'] + list(range(1, rspan+1))), Id=f'{item["name"].replace(" ", "_")}{c}'), rowspan=rspan) + TD(DIV(n, Id=f'{item["name"].replace(" ", "_")}{c}{1}', data_content=min_search_coverage(item['nodes'][c][0] + [item['name']]))) + TD(DIV(', '.join(item['implicit'][c]), data_content=min_search_coverage(item['implicit'][c])), rowspan=rspan))
+					t <= TR(TD(f"{c+1}: " + SELECT((OPTION(x, value=f"{x}") for x in ['Any'] + list(range(1, rspan+1))), Id=f'{item["name"].replace(" ", "_")}{c}', Class='save'), rowspan=rspan) + TD(DIV(n, Id=f'{item["name"].replace(" ", "_")}{c}{1}', data_content=min_search_coverage(item['nodes'][c][0] + [item['name']]))) + TD(DIV(', '.join(item['implicit'][c]), data_content=min_search_coverage(item['implicit'][c])), rowspan=rspan))
 					for idx in range(1, len(item['nodes'][c])):
 						n = ', '.join(item['nodes'][c][idx])
 						nodes[n].append((domain, item['name'], c, idx))
@@ -58,6 +58,13 @@ def init_page():
 			s <= t
 		doc['ascensions'] <= s
 
+	# add core nodes
+	for c_node in ['Force', 'Entropy', 'Form', 'Inertia', 'Life']:
+		t = TABLE(COL(Class='first_column') + COL() + COL(), Class='onehundred borders', data_value=c_node, data_asc_id=f"Core_{c_node}")
+		t <= TR(TH(INPUT(type='checkbox', Id=f'c-Core_{c_node}', Class="save")) + TH(f"Core: {c_node}") + TH(f"Tier 0"))
+		t <= TR(TH() + TH(f"Required: None") + TH(f"Completion: 1 {c_node}"))
+		t <= TR(TH("One of the 5 central nodes.", colspan=3))
+		doc['special'] <= t
 	# set up nodes list
 	t = TABLE(COL(Class='node_column') + COL(), Class='onehundred borders')
 	t <= TR(TH("Node") + TH('Location(s)'))
--- main.py
+++ main.py
@@ -12,22 +12,37 @@ def load_page():
 
 	doc["clear_keywords"].bind("click", clear_keywords)
 
+	# if there is a query string try to load it
+	if any(x in doc.query for x in ['asc', 'nodes']):
+		doc['always_show'].value = 'yes'
+		if 'asc' in doc.query:
+			for asc in doc.query['asc'].split(','):
+				doc[asc].checked = True
+		if 'nodes' in doc.query:
+			for node in doc.query['nodes'].split(','):
+				doc[node[:-1]].value = node[-1]
+
 	# Make it so navigation buttons work
 	@bind('.page', 'click')
 	def change_page(ev):
 		l_val = ev.target['data-id']
 		doc[l_val].style.display = 'block'
-		doc[f'b_{l_val}'].attrs['class'] = 'current_tab page'
+		doc[f'b_{l_val}'].class_name = 'current_tab page'
 		idx = pages.index(l_val)
 		for i in pages[:idx] + pages[idx+1:]:
 			doc[i].style.display = 'none'
-			doc[f'b_{i}'].attrs['class'] = 'page'
+			doc[f'b_{i}'].class_name = 'page'
 
 
 # Function handling filtering changes
 @bind('.save', 'input')
 def save_state(ev):
-	update_page()
+	update_page(ev.target.id, ev.target.type)
+	asc = ','.join([el.id for el in doc.get(selector=":checked") if el.id])
+	asc = 'asc=' + asc if asc else ''
+	node = ','.join([f"{el.id}{el.value}" for el in doc.get(selector="select.save") if el.value != 'Any'])
+	node = 'nodes=' + node if node else ''
+	window.history.replaceState('', '', f"?{asc}{'&' if asc and node else ''}{node}")
 
 
 # Function handling filtering changes
@@ -46,9 +61,9 @@ def update_state(ev):
 			terms = el.attrs['data-content']
 			if all(x in terms for x in search_terms):
 				if 'hidden_class' in el.class_name:
-					del el.attrs['class']
+					del el.class_name
 			else:
-				el.attrs['class'] = 'hidden_class'
+				el.class_name = 'hidden_class'
 	else:
 		update_page()
 
@@ -62,20 +77,35 @@ def clear_keywords(ev):
 
 
 # Set the page visibility state
-def update_page():
-	# Only update visibility if keywords is empty
-	if not doc['keywords'].value:
-		# show all table data
-		for el in doc.get(selector=".hidden_class"):
-			del el.attrs['class']
-
-		always_show = True if doc['always_show'].value == 'no' else False
-		for el in doc.get(selector="[data-asc-id]"):
-			if always_show or doc[f'c-{el.attrs["data-asc-id"]}'].checked:
-				if 'hidden' in el.attrs:
-					del el.attrs['hidden']
+def update_page(tar_id=None, tar_type=None):
+	always_show = True if doc['always_show'].value == 'no' else False
+	if tar_type == 'select-one':
+		el_idx = -1 if doc[tar_id].value == 'Any' else int(doc[tar_id].value)
+		for idx in range(1, len(doc[tar_id].options)):
+			if el_idx < 0 or idx == el_idx:
+				doc[doc[tar_id].id + str(idx)].class_name = doc[doc[tar_id].id + str(idx)].class_name.replace('demphasis', '').strip()
 			else:
-				el.attrs['hidden'] = ''
+				doc[doc[tar_id].id + str(idx)].class_name += ' demphasis'
+	else:
+		# Only update visibility if keywords is empty
+		if not doc['keywords'].value:
+			# show all hidden table data
+			for el in doc.get(selector=".hidden_class"):
+				el.class_name = el.class_name.replace('hidden_class', '').strip()
+			# change cell emphasis based on dropdown selection
+			for el in doc.get(selector="select.save"):
+				el_idx = -1 if el.value == 'Any' else int(el.value)
+				for idx in range(1, len(el.options)):
+					if el_idx < 0 or idx == el_idx:
+						doc[el.id + str(idx)].class_name = doc[el.id + str(idx)].class_name.replace('demphasis', '').strip()
+					else:
+						doc[el.id + str(idx)].class_name += ' demphasis'
+			for el in doc.get(selector="[data-asc-id]"):
+				if always_show or doc[f'c-{el.attrs["data-asc-id"]}'].checked:
+					if 'hidden' in el.attrs:
+						del el.attrs['hidden']
+				else:
+					el.attrs['hidden'] = ''
 
 
 load_page()
