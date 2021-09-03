9df769584594b258a4053adf96080dbf60870783
xanthics <3137623+xanthics@users.noreply.github.com>
2021-09-02 21:06:37 -0700

    Added ascendency keyword descriptions

--- gen_page.py
+++ gen_page.py
@@ -1,5 +1,5 @@
 from browser import document as doc
-from browser.html import TABLE, TR, TH, TD, INPUT, SELECT, SECTION, DIV, OPTION, BR, COL, H1, BUTTON, P
+from browser.html import TABLE, TR, TH, TD, INPUT, SELECT, SECTION, DIV, OPTION, BR, COL, H1, BUTTON, P, UL, LI, STRONG, H2
 from json import load
 from collections import defaultdict
 import re
@@ -18,7 +18,67 @@ def min_search_coverage(items):
 	return ' '.join(ret)
 
 
-def init_page():
+def init_notes():
+	# Basic Ascension notes
+	s = SECTION(P("While Ascension provides many bonuses in terms of basic statistical increases (bonus resistances, Attributes, Abilities, etc.), the various “keywords” it grants represent a significant contribution to character build diversification. Ascension keywords generally function like complex Talents that provide a character with some sort of ability that deeply impacts their mode of play (these effects allow the creation of character builds in the true sense, not just different loadouts). Keywords generally provide a base effect that further investment builds upon with various mutators or additional activation conditions. Below you will find a list of the base effects of the available Ascension keywords."))
+
+	l_items = [
+		["Abeyance:",
+		 "When Abeyance activates, it instantly heals whatever damage it activated from; at the start of your next turn, suffer that amount +15% as unresistable damage, split evenly between your armors and Vitality. Any remaining damage to armor is instead dealt to Vitality.",
+		"Basic activation: If it is not your turn, when you are dealt combat damage equivalent to at least 30% of your maximum Vitality (-0.5% per Inertia or Form embodied, minimum of 20%) at once, Abeyance activates."],
+		["Adaptation:",
+		"When Adaptation is activated, gain or lose up to 30% of your maximum Physical or Magic Armor until their current values equilibrate; loss occurs from the higher of the two values and restoration occurs to the lower of the two values. Additionally, gain a stacking Adaptation status, which lasts for 1 turn, stacks up to 10 and grants +2.5% to FIN, CON, POW, and WIT per stack."
+		"Adaptation skill: 1 AP, no cooldown, activate Adaptation when cast."],
+		["Benevolence:",
+		"When Benevolence is activated, remove 2 stacks of Battered and Harried from the target and move half these stacks (rounded up) onto yourself. Restore 5% of your missing Vitality per stack of Battered or Harried removed this way.",
+		"Gain Mercy, 2 AP, 1 turn cooldown, activate Benevolence on each ally within 8m."],
+		["Celestial:",
+		"1 AP reaction that restores 15% of target's Vitality (+2% per Life embodied).",
+		"This is a Reaction: Reactions are performed automatically when their conditions are satisfied, and can only be performed when it is not the reacting character's turn."],
+		["Centurion:",
+		"1 AP reaction that performs a basic attack on a target within weapon range.",
+		"This is a Reaction: Reactions are performed automatically when their conditions are satisfied, and can only be performed when it is not the reacting character's turn."],
+		["Defiance:",
+		"When you become Flanked, and when you start your turn within 5m of at least two enemies, Defiance activates for 1 turn."],
+		["Elementalist:",
+		"1 AP reaction, deals elemental damage to all characters within 2m of the reaction's target. When you perform an Elementalist reaction, suffer damage of the reaction's type, equivalent to 10% of your maximum Vitality for each stack of Elementalist you have. Then gain 1 stack of Elementalist, or 2 if using the same element twice in a row.",
+		"This is a Reaction: Reactions are performed automatically when their conditions are satisfied, and can only be performed when it is not the reacting character's turn."],
+		["Occultist:",
+		"1 AP reaction, apply Bane for 2 turns. Bane deals Physical damage to the target at the beginning of their turn. When Bane ends, it deals its base damage +5% per Battered or Harried that the target suffered over its duration.",
+		"This is a Reaction: Reactions are performed automatically when their conditions are satisfied, and can only be performed when it is not the reacting character's turn."],
+		["Paucity:",
+		"While active, Paucity grants +15% lifesteal (+2% per Entropy).",
+		"Basic activation: Once per round, when you drop to 25% Vitality or below, activate Paucity."],
+		["Predator:",
+		"1 AP reaction that performs a basic attack on a target within weapon range. If this attack is performed with a melee weapon, it has +30% critical chance. Additionally, if this attack is performed with a dagger, attempt to sneak afterward.",
+		"This is a Reaction: Reactions are performed automatically when their conditions are satisfied, and can only be performed when it is not the reacting character's turn."],
+		["Presence:",
+		"Presence is considered active on any allies that are affected by your Leadership."],
+		["Prosperity:",
+		"Basic activation: When you have at least 90% Vitality (-1% per Form or Life embodied), Prosperity is considered active."],
+		["Purity:",
+		"When Purity is activated, instantly heal 15% (+1.5% per Life embodied) of missing Vitality and armors - also gain a 10m radius aura for 2 turns that restores 8% (+1.5% per Life embodied) of missing Vitality, Magic, and Physical Armor per turn to yourself and allies. 4 turn Cooldown.",
+		"Basic activation: When reduced to 30% Vitality (+1.5% per Life embodied) or below, activate Purity."],
+		["Violent Strike:",
+		"If Violent Strike is active when you use a skill, basic attack or reaction, perform a Violent Strike on the first enemy you hit, which deals 30% of your weapon damage to enemies within 2m of that target."],
+		["Vitality Void:",
+		"When Vitality Void is activated, deal piercing damage, split between all enemies within 3m of you, equal to 11% of your maximum Vitality (+1% per Entropy embodied), and you heal for 15% (+2% per Entropy embodied) of this damage. Vitality Void is not affected by increases or reductions to your damage.",
+		"Basic activation: Whenever you spend a Source Point in combat, activate Vitality Void."],
+		["Volatile Armor:",
+		"Whenever you suffer damage to armor of the designated type, deal the listed percentage of that damage to all characters within range, as whatever damage type is noted."],
+		["Voracity:",
+		"Whenever an enemy dies in combat, your Voracity effects are granted to you. If the dying enemy was a summon, you are instead granted only 20% of these effects' intended values."],
+		["Ward:",
+		"When Ward is active, gain +10% to all resistances.",
+		"Basic activation: After you have suffered total damage exceeding 60% of your total Vitality (-0.5% per Constitution over 10, minimum of 20%), activate Ward for 1 turn."],
+		["Wither: ",
+		"Withered reduces the effect of Power by 15% (+2% per your Entropy embodied). Additionally, once per round per target, Wither applies 1 Battered and Harried as it is applied."]
+	]
+
+	for li in l_items:
+		s <= H2(li[0]) + UL(LI(x) for x in li[1:])
+	doc['notes'] <= s
+
 	# Note about Pip and Derpy mod changes
 	pip_generic = [
 		"<strong>Any note in this color is specific to Pip's Mod.</strong>  Current as of 30/08/2021.",
@@ -62,6 +122,9 @@ T1 Ascension effects that granted 2 AP recovery on weapon skills are now unable 
 		s <= P(d)
 	doc['notes'] <= s
 
+
+def init_page():
+	init_notes()
 	# keep track of ascendancy resources needed/granted
 	# set up page search functions
 	always_show = SELECT(Id=f"always_show", Class=f"filter onehundred")
